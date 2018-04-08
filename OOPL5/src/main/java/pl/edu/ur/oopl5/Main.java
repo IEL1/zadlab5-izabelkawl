161418331d3fae3f17fb05ac41388b1e1c0f06d4
Izabela <Izabela@Iza-Komputer>
2018-04-08 19:30:39 +0200

    Zadania 3,5,6

--- OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
@@ -1,11 +1,92 @@
 package pl.edu.ur.oopl5;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //Zadanie2
-     
-        // TODO zad 3, 5, 6
+
+        System.out.println("Zadanie3");
+        int p, n, m, j, c, i = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj wysokońć stosu: ");
+        p = scanner.nextInt();
+        zad3 stos = new zad3(p);
+
+        do {
+            System.out.println();
+            System.out.println("Dołóż wartość na stos - wciśnij 1");
+            System.out.println("Zdejmij wartość ze stosu -wciśnij 2");
+            System.out.println("Pokaż wartość na szczycie stosu - wciśnij 3");
+            System.out.println("Wyjdź - wciśnij 0");
+            n = scanner.nextInt();
+
+            switch (n) {
+                case 1:
+                    if (!stos.isFull()) {
+                        System.out.print("Podaj liczbę:");
+                        m = scanner.nextInt();
+                        stos.push(m);
+                    } else {
+                        System.out.println("Stos jest pełny");
+                    }
+                    break;
+
+                case 2:
+                    if (!stos.isEmpty()) {
+                        System.out.println(stos.pop() + " zostało zdjęte ze stosu!");
+                    } else {
+                        System.out.println("Stos jest pusty");
+                    }
+                    break;
+                case 3:
+                    stos.top();
+                    break;
+
+                case 0:
+                    System.out.println("Koniec działania programu");
+                    break;
+
+                default:
+                    System.out.println("Zła liczba, powrót do wyboru");
+            }
+        } while (n != 0);
+
+        System.out.println("Zadanie5");
+        firma_zad5 firma = new firma_zad5();
+        do {
+            String[][] x = firma.dane;
+            System.out.println();
+            System.out.println("1. Wprowadź pracownika");
+            System.out.println("2. Wyświetl pracownika");
+            System.out.println("0. Wyjdź");
+            c = scanner.nextInt();
+
+            switch (c) {
+                case 1:
+                    firma.dodajPracownika(i, x);
+                    if (i < 100) {
+                        i++;
+                    }
+                    break;
+
+                case 2:
+
+                    firma.wyswietlPracownikow(i, x);
+                    break;
+
+                case 0:
+                    System.out.println("Koniec dziełania programu");
+                    break;
+
+                default:
+                    System.out.println("Zła liczba");
+            }
+        } while (c != 0);
+
+        System.out.println("Zadanie6");
+        zad6 zad6 = new zad6();
+        zad6.menu();
     }
-    
 }
---
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/firma_zad5.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Izabela
+ */
+public class firma_zad5 {
+
+    private String wiekstring;
+    public String[][] dane = new String[100][4];
+    Scanner scanner = new Scanner(System.in);
+
+    
+    
+    String[][] dodajPracownika(int i, String[][] dane) {
+        if(i<100){
+            pracownik_zad5 pracownik = new pracownik_zad5();
+            System.out.println(); 
+            System.out.println("Podaj dane pracownika nr. " + (i+1));                    
+            System.out.print("Imię: ");
+            pracownik.imie=scanner.next();
+            dane[i][0]=pracownik.imie;
+            System.out.print("Nazwisko: ");
+            pracownik.nazwisko=scanner.next();
+            dane[i][1]=pracownik.nazwisko;
+            System.out.print("Wiek: ");
+            pracownik.wiek=scanner.nextInt();
+            wiekstring=Integer.toString(pracownik.wiek);
+            dane[i][2]=wiekstring;
+            System.out.print("Stanowisko: ");
+            pracownik.stanowisko=scanner.next();
+            dane[i][3]=pracownik.stanowisko;
+            i++;
+        }
+        else{
+            System.out.println("Lista pracowników jest pełna");
+        }
+        return dane;
+    }
+
+    
+    
+    void wyswietlPracownikow(int i, String[][] dane) {
+        if(i>0){
+            for (int j = 0; j < i; j++) {
+                System.out.println(); 
+                System.out.println("Pracownik nr. " + (j+1));                    
+                System.out.println("Imię: " + dane[j][0]);
+                System.out.println("Nazwisko: " + dane[j][1]);
+                System.out.println("Wiek: " + dane[j][2]);
+                System.out.println("Stanowisko: " + dane[j][3]);
+                
+            }
+        }
+        else{
+            System.out.println("Brak pracowników do wyświetlenia");
+        }
+    }
+}
